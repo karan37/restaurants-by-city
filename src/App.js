04402bf90db4689b7748d12db1fe8d5d3402fdf1
k37singh <k37singh@gmail.com>
2020-07-02 03:32:07 -0400

    dusra

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React (Ab ye na bol)
         </a>
       </header>
     </div>
